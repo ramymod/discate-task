b665fc2ec11cc5fee2972fbecdd4a61aa2d2f658
ramymod <113202925+ramymod@users.noreply.github.com>
2022-12-19 19:52:06 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,75 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prime_num
+namespace perfect_num
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i, j ;
 
-            
-            Console.WriteLine("Enter n1: ");
+
+            Console.WriteLine("Enter n1 : ");
             int n1 = int.Parse(Console.ReadLine());
-
-
-            
-            Console.WriteLine("Enter n2: ");
+            Console.WriteLine("Enter n2 : ");
             int n2 = int.Parse(Console.ReadLine());
 
+            Console.WriteLine(" the perfect number between {0} and {1} are", n1, n2);
 
-
-            Console.WriteLine("prime numbers betwen {0} and {1} is :", n1, n2);
-
-
-            if (n1 <= 2)
+            for (int i = n1; i <= n2; i++)
             {
-                n1 = 2;
-                if (n2 >= 2)
-                {
-                    Console.WriteLine ( n1 );
-                    n1++;
-                }
-
-            }
-
-           
-            if (n1 % 2 == 0)
-                n1 ++;
-
-        
-            for (i = n1; i <= n2; i = i + 2)
-            {
-
-                bool flag = true ;
-
-                for (j = 2; j * j <= i; ++j)
+                int sum = 0;
+                for (int j = 1; j < i; j++)
                 {
                     if (i % j == 0)
-                    {
-                        flag = false;
-                        break;
-                    }
+                        sum = sum + j;
                 }
-
-               
-                if (flag == true)
-                {
-                    if (i == 1)
-                        continue;
-                    else
-                        Console.WriteLine(i);
-                }
+                if (sum == i)
+                    Console.WriteLine(i);
             }
-            Console.ReadKey();
 
-   
-        }
+            Console.ReadKey();
 
 
     }
-        
-
-    
+    }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("prime num")]
+[assembly: AssemblyTitle("perfect num")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("prime num")]
+[assembly: AssemblyProduct("perfect num")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4df2c684-79f6-4157-a63a-8d63fd03e8c2")]
+[assembly: Guid("672aa0af-8cca-408b-8012-1f6806856a8a")]
 
 // Version information for an assembly consists of the following four values:
 //
